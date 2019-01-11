679b18d3e19d43fd83c993070b4cc757416d6802
Adrien Eudes <adrien.eudes@gmail.com>
2019-01-11 15:13:43 +0100

    save to storage

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const async = require('async')
+const fs = require('fs')
+const {Storage} = require('@google-cloud/storage')
+const projectId = 'ae-lab'
+
+const storage = new Storage({
+    projectId: projectId,
+})
+
+const bucketName = 'gh-cf-link'
 
 exports.helloWorld = (req, res) => {
     console.log(JSON.parse(req.body.payload))
-    async.parallel([callback => {
-        callback(null, 1)
-    }], (_, resp) => {
-        res.json(resp)
+    fs.writeFileSync('/tmp/test.txt', req.body.payload)
+    storage.bucket(bucketName).upload('/tmp/test.txt', {}).then(() => {
+        async.parallel([callback => {
+            callback(null, 1)
+        }], (_, resp) => {
+            res.json(resp)
+        })
     })
+
+    
 }
 
